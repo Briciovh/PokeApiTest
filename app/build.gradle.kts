0ec11eb90c2547d83df3c1a29d1052d56b7cec0b
Bricio Velázquez <briciovh@hotmail.com>
2026-03-20 23:23:24 -0400

    Adds Room DB to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,11 @@ dependencies {
     // Icons
     implementation(libs.androidx.compose.material.icons.extended)
 
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
     // Testing
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
