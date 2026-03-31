03272c18d11cfb0c30106894cbf38ee89827ac62
Bricio Velázquez <briciovh@hotmail.com>
2026-03-31 14:17:36 -0400

    Varieties updates

--- app/src/main/java/com/example/pokeapitest/data/local/PokemonDatabase.kt
+++ app/src/main/java/com/example/pokeapitest/data/local/PokemonDatabase.kt
@@ -7,7 +7,7 @@ import com.example.pokeapitest.data.local.entity.PokemonListItemEntity
 
 @Database(
     entities = [PokemonListItemEntity::class, PokemonEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class PokemonDatabase : RoomDatabase() {
